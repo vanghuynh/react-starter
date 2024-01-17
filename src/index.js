23c739194dcef321d3a0cc18bed84df3d9fdeb8d
vhuynh <vhuynh@gmail.com>
2024-01-17 11:16:52 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// create a new component. this component should produce some HTML
+const App = () => {
+  return <div>Hi!</div>;
+};
+
+// take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector(".container"));
